e1832e696bd149f06339ef9aeaf0df4d71bb607d
Dmitry Sedelnikov <dsedelnikov@thumbtack.net>
2013-04-01 19:15:05 +0700

    Upload by jquery uploader.

---
+++ public/js/profile.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+    var form = $('.form-upload'),
+        title = $('#image_title'),
+        file_input = $('#image'),
+        upload_block = $('.image-upload');
+
+    form.on('submit', function(e) {
+        e.preventDefault();
+    })
+
+    file_input.fileupload({
+        dataType: 'json',
+        add: function (e, data) {
+            form.data('file', data.files[0]);
+            upload_block.find('span').text(data.files[0].name);
+            form.find('[type="submit"]').click(function () {
+                if($.trim(title.val())) {
+                    data.submit();
+                }
+            });
+        },
+        stop: function (e, data) {
+            window.location.href = window.location.href;
+        },
+        send: function (e, data) {
+           if(form.data('file') != data.files[0]){
+               return false;
+           }
+        }
+    });
+});
