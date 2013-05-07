a39ee606cedcafd7f5dd2d5d3b80b8f9e8cbee80
Dmitry Sedelnikov <dsedelnikov@thumbtack.net>
2013-05-07 11:36:46 +0700

    Refactoring of config.

--- app/config/config.js
+++ app/config/config.js
@@ -1,6 +1,5 @@
 
 module.exports = {
-
     development: {
         app: {
             name: 'Imagerate dev'
@@ -71,25 +70,6 @@ module.exports = {
                 'aremnev@thumbtack.net'
             ]
         }
-    },
-
-    buildMongoUrl: function(mongo) {
-        if (!mongo) {
-            var env = JSON.parse(process.env.VCAP_SERVICES);
-            mongo = env['mongodb-1.8'][0]['credentials'];
-        }
-
-        mongo.hostname = (mongo.hostname || 'localhost');
-        mongo.port = (mongo.port || 27017);
-        mongo.db = (mongo.db || 'test');
-
-        if(mongo.username && mongo.password){
-            return 'mongodb://' + mongo.username + ':' + mongo.password + '@' + mongo.hostname + ':' + mongo.port + '/' + mongo.db;
-        }
-        else{
-            return 'mongodb://' + mongo.hostname + ':' + mongo.port + '/' + mongo.db;
-        }
     }
-
 }
 
---
+++ app/config/mongo.js
@@ -0,0 +1,27 @@
+
+var mongoose = require('mongoose');
+
+
+module.exports = function (app, config) {
+
+    config.db = buildMongoUrl(config.mongo);
+    mongoose.connect(config.db);
+
+    function buildMongoUrl(mongo) {
+        if (!mongo) {
+            var env = JSON.parse(process.env.VCAP_SERVICES);
+            mongo = env['mongodb-1.8'][0]['credentials'];
+        }
+
+        mongo.hostname = (mongo.hostname || 'localhost');
+        mongo.port = (mongo.port || 27017);
+        mongo.db = (mongo.db || 'test');
+
+        if(mongo.username && mongo.password){
+            return 'mongodb://' + mongo.username + ':' + mongo.password + '@' + mongo.hostname + ':' + mongo.port + '/' + mongo.db;
+        }
+        else{
+            return 'mongodb://' + mongo.hostname + ':' + mongo.port + '/' + mongo.db;
+        }
+    }
+}
