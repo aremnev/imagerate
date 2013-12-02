0886aa2081f933a0bdc51c0f68bb7c1279196189
Andrey Ivanov <aIvanov@thumbtack.net>
2013-12-02 14:45:16 +0700

    little fix

--- app/models/image.js
+++ app/models/image.js
@@ -262,7 +262,7 @@ ImageSchema.statics = {
     /**
      * returns prev image for the same contest
      */
-    prev : function(image, auth, cb){
+    prev : function(image, cb){
         this.findOne()
             .where('contest.contest').equals(image.contest.contest)
             .where('createdAt').lt(image.createdAt)
